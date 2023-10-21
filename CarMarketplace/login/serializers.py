ee5c3547b277e0672c9e9802f0ba0e4134afb35b
Matheus Coitinho <mhcl0303@gmail.com>
2023-10-21 16:13:09 -0300

    fix Json do cliente agora contém todas as infos do User

--- CarMarketplace/login/serializers.py
+++ CarMarketplace/login/serializers.py
@@ -1,7 +1,14 @@
 from .models import Cliente
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User
+        fields = '__all__'
 
 class ClienteSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True)
     class Meta:
         model = Cliente
         fields = '__all__'
