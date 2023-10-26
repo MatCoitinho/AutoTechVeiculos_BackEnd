b840b6a032edb8b74709d634b736a01047b7b874
Matheus Coitinho <mhcl0303@gmail.com>
2023-10-26 14:30:25 -0300

    fix correção no json da Solicitacao

--- CarMarketplace/solicitarAnuncio/serializers.py
+++ CarMarketplace/solicitarAnuncio/serializers.py
@@ -2,6 +2,10 @@ from .models import Solicitacao
 from rest_framework import serializers
 
 class SolicitacaoSerializer(serializers.ModelSerializer):
+    dono_cpf = serializers.CharField(source='solicitante.cpf',read_only=True)
+    dono_email = serializers.CharField(source='solicitante.user.email',read_only=True)
+    dono_name = serializers.CharField(source='solicitante.user.first_name',read_only=True)
+    dono_telefone = serializers.CharField(source='solicitante.telefone',read_only=True)
     class Meta:
         model = Solicitacao
         fields = '__all__'
