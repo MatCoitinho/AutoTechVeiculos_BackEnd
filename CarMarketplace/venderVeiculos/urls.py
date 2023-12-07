6b979601ed32dcf3eb7b48bc23eef0f27495910f
Matheus Coitinho <mhcl0303@gmail.com>
2023-12-07 00:27:54 -0300

    [FIX]Endpoint de venda

--- CarMarketplace/venderVeiculos/urls.py
+++ CarMarketplace/venderVeiculos/urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import VendaViewSet
+from .views import VendaViewSet, criar_venda
 from django.urls import path, include
+
 
 router = DefaultRouter()
 router.register(r'Venda', VendaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('criarVenda',criar_venda,name='Vender')
 ]
